736e2f1c9c638f49f2d00e645d8fba56c14d7e71
rubydevi <ariesabyss@gmail.com>
2023-06-09 16:44:55 +0530

    Export in a way that constructor can be accessible

--- modules/book.js
+++ modules/book.js
@@ -1,9 +1,7 @@
 /* eslint linebreak-style: ["error", "windows"] */
-class Book {
+export default class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 }
-
-export default { Book }
